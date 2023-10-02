2d1d862289d65a0ae9f425538b98f2c2910df248
Alex Fix <dasdias@yandex.ru>
2023-10-02 18:03:23 +0300

    create html structure

---
+++ main.js
@@ -0,0 +1,36 @@
+import "normalize.css"
+import './style.scss'
+// import Swiper JS
+import { Navigation, Thumbs } from 'swiper/modules';
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+
+var swiperThumbnails = new Swiper(".product__slider-thumbnails", {
+	spaceBetween: 10,
+	slidesPerView: 4,
+	freeMode: true,
+	watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".product__slider-main", {
+	spaceBetween: 10,
+	navigation: {
+		nextEl: ".product__arrow-next",
+		prevEl: ".product__arrow-prev",
+	},
+	modules: [Navigation, Thumbs],
+	thumbs: {
+		swiper: swiperThumbnails,
+	},
+});
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//    <h1>Hello js</h1>
+//   </div>
+// `
