7ef06d81554ea9e2a8419f4d0e6d542e81114491
Alex Fix <dasdias@yandex.ru>
2023-10-31 17:43:22 +0300

    day 6 begin

--- main.js
+++ main.js
@@ -9,113 +9,115 @@ import { ProductList } from "./modules/ProductList/ProductList";
 import { ApiService } from "./services/ApiService";
 import { Catalog } from "./modules/Catalog/Catalog";
 import { NotFound } from "./modules/NotFound/NotFound";
+import { FavoriteService } from "./services/StorageService";
 
 const productSlider = () => {
-  Promise.all([
-    import('swiper/modules'),
-    import('swiper'),
-    import('swiper/css')
-  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+	Promise.all([
+		import('swiper/modules'),
+		import('swiper'),
+		import('swiper/css')
+	]).then(([{ Navigation, Thumbs }, Swiper]) => {
 
-    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
+		const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+			spaceBetween: 10,
+			slidesPerView: 4,
+			freeMode: true,
+			watchSlidesProgress: true,
+		});
 
-    const swiper2 = new Swiper.default(".product__slider-main", {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".product__arrow_next",
-        prevEl: ".product__arrow_prev",
-      },
-      modules: [Navigation, Thumbs],
-      thumbs: {
-        swiper: swiperThumbnails,
-      },
-    });
+		const swiper2 = new Swiper.default(".product__slider-main", {
+			spaceBetween: 10,
+			navigation: {
+				nextEl: ".product__arrow_next",
+				prevEl: ".product__arrow_prev",
+			},
+			modules: [Navigation, Thumbs],
+			thumbs: {
+				swiper: swiperThumbnails,
+			},
+		});
 
-  })
+	})
 }
 
 const init = () => {
-  const api = new ApiService();
-  const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
+	const api = new ApiService();
+	const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
 
 
-  new Header().mount();
-  new Main().mount();
-  new Footer().mount();
+	new Header().mount();
+	new Main().mount();
+	new Footer().mount();
 
-  api.getProductCategories().then((data) => {
-    new Catalog().mount(new Main().element, data);
-    router.updatePageLinks();
-  })
-
-
-  productSlider();
+	api.getProductCategories().then((data) => {
+		new Catalog().mount(new Main().element, data);
+		router.updatePageLinks();
+	})
 
 
-  router.on("/", async () => {
-    const product = await api.getProducts();
-    new ProductList().mount(new Main().element, product, "Товары")
-    router.updatePageLinks();
-  }, {
-    leave(done) {
-      new ProductList().unmount()
-      done()
-    },
-    already() {
+	productSlider();
 
-    }
-  })
-    .on("/category", async ({ params: { slug } }) => {
-      const product = await api.getProducts();
-      new ProductList().mount(new Main().element, product, slug)
-      router.updatePageLinks();
-    }, {
-      leave(done) {
-        new ProductList().unmount()
-        done()
-      }
-    })
-    .on("/favorite", async () => {
-      const product = await api.getProducts();
-      new ProductList().mount(new Main().element, product, "Избранное")
-      router.updatePageLinks();
-    }, {
-      leave(done) {
-        new ProductList().unmount()
-        done()
-      }
-    })
-    .on("/search", () => {
-      console.log('search');
-    })
-    .on("/product/:id", (obj) => {
-      console.log(obj);
-    })
-    .on("/cart", () => {
-      console.log('cart');
-    })
-    .on("/order", () => {
-      new Order().mount(new Main().element);
-      console.log('order');
-    })
-    .notFound(() => {
-      new NotFound().mount(new Main().element)
-      setTimeout(() => {
-        router.navigate('/');
-      }, 5000);
-    }, {
-      leave(done) {
-        new Main().element.textContent = '';
-        done()
-      }
-    })
-  router.resolve();
-  new Footer().mount();
+
+	router.on("/", async () => {
+		const products = await api.getProducts();
+		new ProductList().mount(new Main().element, products, "Товары")
+		router.updatePageLinks();
+	}, {
+		leave(done) {
+			new ProductList().unmount()
+			done()
+		},
+		already() {
+
+		}
+	})
+		.on("/category", async ({ params: { slug } }) => { // slug - это то, что в search параметре
+			const { data: products } = await api.getProducts({ category: slug }); // data: products - переименовываем переменную
+			new ProductList().mount(new Main().element, products, slug)
+			router.updatePageLinks();
+		}, {
+			leave(done) {
+				new ProductList().unmount()
+				done()
+			}
+		})
+		.on("/favorite", async () => {
+			const favorite = new FavoriteService().get();
+			const { data: product } = await api.getProducts({ list: favorite.join(",") });
+			new ProductList().mount(new Main().element, product, "Избранное")
+			router.updatePageLinks();
+		}, {
+			leave(done) {
+				new ProductList().unmount()
+				done()
+			}
+		})
+		.on("/search", () => {
+			console.log('search');
+		})
+		.on("/product/:id", (obj) => {
+			console.log(obj);
+		})
+		.on("/cart", () => {
+			console.log('cart');
+		})
+		.on("/order", () => {
+			new Order().mount(new Main().element);
+			console.log('order');
+		})
+		.notFound(() => {
+			new NotFound().mount(new Main().element)
+			setTimeout(() => {
+				router.navigate('/');
+			}, 5000);
+		}, {
+			leave(done) {
+				new Main().element.textContent = '';
+				done()
+			}
+		})
+	router.resolve();
+	new Footer().mount();
 }
 
 init();
